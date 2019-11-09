546c2ed903b24df8230e6473118f17d3cb356aeb
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-09 15:49:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(speak)
   if speak == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif speak == speak.upcase
     puts "NO, NOT SINCE 1938!"
   else
@@ -10,3 +10,7 @@ end
 end
 
 speak_to_grandma('I LOVE YOU GRANDMA!')
+speak_to_grandma('Hi Nana, how are you?')
+speak_to_grandma('Hi!')
+speak_to_grandma('WHAT DI YOU WAT TODAY?')
+speak_to_grandma('WHAT?')
