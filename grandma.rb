5ad46d613f9324642c1633eeec5d06dc86d80cb0
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-09 15:47:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,10 @@ def speak_to_grandma(speak)
   if speak == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN"
   elseif speak == speak.upcase
-    puts "N"
+    puts "NO, NOT SINCE 1938!"
+  else
+    puts "HUH?! SPEAK UP, SONNY!"
 end
 end
+
+speak_to_grandma('I LOVE YOU GRANDMA!')
